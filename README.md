b3f2b81786cca319d50fa568f0dc12f338035f48
AnInsaneJimJam <anand47bansal@gmail.com>
2026-01-10 01:56:28 +0530

    Added Readme and setup commands

---
+++ README.md
@@ -0,0 +1,5 @@
+# Rusty Git
+
+### Rust implementation of git version control system
+
+Purpose of this project is sole learning. Although compatible with git, it is not advisable to be used as your primary version control system. For forking and experimental purposes, you are welcome my friend
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::env;
+
+enum Command{
+    Add,
+    CatFile,
+    CheckIgnore,
+    Checkout,
+    Commit,
+    HashObject,
+    Init,
+    Log,
+    LsFiles,
+    LsTrees,
+    RevParse,
+    Rm,
+    ShowRef,
+    Status,
+    Tag,
+}
+
 fn main() {
-    
+    let args: Vec<String> = env::args().collect();
 }
